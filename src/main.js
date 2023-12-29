9b828b7154beca1e80d4adc76b6750c664774db7
Alexis Liao <Alexisliao10@hotmail.com>
2023-12-29 17:20:40 -0500

    Animation added to hamMenu
    HamMenu button will do a subtle animation when open or close

--- src/main.js
+++ src/main.js
@@ -15,7 +15,6 @@ const api = axios.create({
 async function getTrendingMoviesPreview() {
   const { data } = await api("trending/movie/day");
   const trendMovies = data.results;
-  console.log(trendMovies);
 
   trendMovies.slice(0, 10).forEach((movie) => {
     const trendMoviesContainer = document.querySelector("#trendMovies");
@@ -40,3 +39,32 @@ async function getTrendingMoviesPreview() {
 }
 
 getTrendingMoviesPreview();
+const hamMenu = document.querySelector("nav .hamMenu");
+const hamIcon = document.querySelector(".hamMenu .fa-bars");
+const moviesContainer = document.querySelector("body > main > section");
+
+hamMenu.addEventListener("click", toggleHamMenuView);
+
+function toggleHamMenuView() {
+  const quitIcon = document.querySelector(".hamMenu .fa-xmark");
+  const menu = document.querySelector("main div ul");
+  const divContainer = document.querySelector("body > main > div");
+  const inputContainer = document.querySelector("body > main > div > div");
+  const sectionTrending = document.querySelector("body > main > section");
+
+  hamIcon.classList.toggle("hidden");
+  quitIcon.classList.toggle("hidden");
+  menu.classList.toggle("translate-y-0");
+  divContainer.classList.toggle("h-52");
+  divContainer.classList.toggle("h-12");
+  inputContainer.classList.toggle("translate-y-40");
+  sectionTrending.classList.toggle("opacity-50");
+}
+
+moviesContainer.addEventListener("click", closeHamMenu);
+
+function closeHamMenu() {
+  if (hamIcon.classList.contains("hidden")) {
+    toggleHamMenuView();
+  }
+}
