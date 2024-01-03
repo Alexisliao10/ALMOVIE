bf7a813230d5977b0c0a923d959a1ac3cfc61628
Alexis Liao <Alexisliao10@hotmail.com>
2024-01-03 16:47:41 -0500

    Moved renderTrendMovies function to trendMovies.js
    This will prevent the files from incurring circular dependencies

--- src/main.js
+++ src/main.js
@@ -36,31 +36,3 @@ function closeHamMenu() {
   toggleHamMenuView();
 }
 nodes.moviesContainer.addEventListener("click", closeHamMenu);
-
-export async function renderTrendMovies(range) {
-  try {
-    const trendMovies = await getTrendMovies(range);
-    trendMovies.slice(0, 10).forEach((movie) => {
-      const trendMoviesContainer = document.querySelector("#trendMovies");
-
-      const movieCard = document.createElement("figure");
-      // const link = document.createElement("a");
-      const movieImg = document.createElement("img");
-      const movieTitle = document.createElement("figcaption");
-      movieImg.classList.add(
-        "rounded-lg",
-        "active:border-2",
-        "active:border-sky-500",
-      );
-      movieImg.src = `https://image.tmdb.org/t/p/w185${movie.poster_path}`;
-      movieImg.alt = movie.title;
-      movieTitle.textContent = movie.title;
-      movieTitle.classList.add("w-full", "text-center", "mt-2");
-
-      movieCard.append(movieImg, movieTitle);
-      trendMoviesContainer.append(movieCard);
-    });
-  } catch (error) {
-    throw new Error(error.message);
-  }
-}
