e184ae2679248a203560e85bf82b8bb62643aff6
Alexis Liao <Alexisliao10@hotmail.com>
2024-02-20 21:20:43 -0500

    Refactor - loading skeleton animation use less code
    Changed custom animation property to tailwindCss "animate-pulse".

--- src/pages/loading.js
+++ src/pages/loading.js
@@ -5,22 +5,24 @@ export function mainLoadingSkeleton() {
   const text = document.createElement("div");
 
   card.classList.add(
-    "skeleton",
+    "animate-pulse",
     "relative",
     "mb-9",
     "h-[250px]",
     "w-[150px]",
     "max-w-xs",
     "rounded-lg",
+    "bg-[#6b7280]/[0.5]",
   );
   text.classList.add(
-    "skeleton",
+    "animate-pulse",
     "absolute",
     "-bottom-8",
     "left-7",
     "h-5",
     "w-24",
     "rounded-lg",
+    "bg-[#6b7280]/[0.5]",
   );
   card.append(text);
 
